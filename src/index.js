e49e80957f89881500c945e9a51e88d319f3ca61
Aleksei Ovchinnikov <eastonsc6@yandex.ru>
2022-04-16 19:04:47 +0300

    del main custom scroll

--- src/index.js
+++ src/index.js
@@ -2,18 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import Scrollbar from 'react-scrollbars-custom';
-
-const scrollBarStyles = {
-  width: '100%',
-  height: '100vh'
-};
 
 ReactDOM.render(
   <React.StrictMode>
-    <Scrollbar noScrollX style={scrollBarStyles}>
-      <App/>
-    </Scrollbar>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
